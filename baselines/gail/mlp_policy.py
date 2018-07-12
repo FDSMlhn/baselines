bd3d5a0d84bed5386b3cc34dd5154e79a0d91343
Nevermore_Lhn <FDSM_lhn@yahoo.com>
2018-07-12 13:08:14 +0800

    Update mlp_policy.py

--- baselines/gail/mlp_policy.py
+++ baselines/gail/mlp_policy.py
@@ -26,10 +26,13 @@ class MlpPolicy(object):
         assert isinstance(ob_space, gym.spaces.Box)
 
         self.pdtype = pdtype = make_pdtype(ac_space)
-        sequence_length = None
+        sequence_length = None  #None is for variable length of observation
 
+        
+        #ob_space (11,)
         ob = U.get_placeholder(name="ob", dtype=tf.float32, shape=[sequence_length] + list(ob_space.shape))
-
+        
+        
         with tf.variable_scope("obfilter"):
             self.ob_rms = RunningMeanStd(shape=ob_space.shape)
 
@@ -42,10 +45,13 @@ class MlpPolicy(object):
         last_out = obz
         for i in range(num_hid_layers):
             last_out = tf.nn.tanh(dense(last_out, hid_size, "polfc%i" % (i+1), weight_init=U.normc_initializer(1.0)))
-
+        
         if gaussian_fixed_var and isinstance(ac_space, gym.spaces.Box):
+            #pdtype.param_shape() is 6
             mean = dense(last_out, pdtype.param_shape()[0]//2, "polfinal", U.normc_initializer(0.01))
             logstd = tf.get_variable(name="logstd", shape=[1, pdtype.param_shape()[0]//2], initializer=tf.zeros_initializer())
+            
+            #this shape will be 6
             pdparam = tf.concat([mean, mean * 0.0 + logstd], axis=1)
         else:
             pdparam = dense(last_out, pdtype.param_shape()[0], "polfinal", U.normc_initializer(0.01))
